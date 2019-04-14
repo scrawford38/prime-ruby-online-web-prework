96acaab2cebea0949d9125088f8e09706a61f78c
Sally Crawford <sally.crawford@gmail.com>
2019-04-14 15:21:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
 def prime?(value)
   if value > 1
     range = (2..value-1).to_a
-    range.none? do |range_num| 
+    range.none? do |range_num|
       value % range_num == 0
     end
   else
     false
   end
 end
-  
   
